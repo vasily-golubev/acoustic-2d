84e4b3c8aa4d119f541e669df92e314deca68654
Vasily Golubev <w.golubev@mail.ru>
2020-11-05 16:51:08 +0300

    Added material and good parameters

--- solution.py
+++ solution.py
@@ -15,13 +15,15 @@
 	(0,0)
 '''
 
-time = 1 # Total time in seconds
-dt = 0.01 # Time step in seconds
+# Courant condition: c * dt < min{dx, dy}
+
+time = 0.1 # Total time in seconds
+dt = 0.003 # Time step in seconds
 nt = int(time / dt) # Amount of time steps
-lx = 1 # Length along X in meters
-ly = 1 # Length along Y in meters
-dx = 0.1 # Spatial step along X in meters
-dy = 0.1 # Spatial step along Y in meters
+lx = 200 # Length along X in meters
+ly = 100 # Length along Y in meters
+dx = 5 # Spatial step along X in meters
+dy = 5 # Spatial step along Y in meters
 nx = int(lx / dx) # Number of nodes along X
 ny = int(ly / dy) # Number of nodes along Y
 
@@ -29,6 +31,9 @@ ny = int(ly / dy) # Number of nodes along Y
 
 data_curr = [[0, 0, 0] * ny for _ in range(nx)] # n-th time layer data
 data_next = [[0, 0, 0] * ny for _ in range(nx)] # (n+1)-th time layer data
+
+# with possible extension to the mat(x,y) dependency
+mat = {'rho': 997, 'c': 1435} # medium material rho [kg/m^3], c [m/s]
 
 def apply_initial_data():
 	pass
