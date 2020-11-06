b9a50cf62afb815da5d8d18ab0e9eeb79792ee4f
Vasily Golubev <w.golubev@mail.ru>
2020-11-06 13:44:33 +0300

    Advection now works.

--- advection.py
+++ advection.py
@@ -23,12 +23,12 @@ import sys
 
 u0 = 1 # initial u value
 time = 0.015 # Total time in seconds
-dt = 0.003 # Time step in seconds
+dt = 0.0006 # Time step in seconds
 nt = int(time / dt) # Amount of time steps
 lx = 200 # Length along X in meters
 ly = 100 # Length along Y in meters
-dx = 5 # Spatial step along X in meters
-dy = 5 # Spatial step along Y in meters
+dx = 1 # Spatial step along X in meters
+dy = 1 # Spatial step along Y in meters
 nx = int(lx / dx) # Number of nodes along X
 ny = int(ly / dy) # Number of nodes along Y
 
@@ -39,11 +39,16 @@ data_curr = [[0] * nx for _ in range(ny)] # n-th time layer data
 data_next = [[0] * nx for _ in range(ny)] # (n+1)-th time layer data
 
 # with possible extension to the mat(x,y) dependency
-mat = {'cx': 1450, 'cy': 1450} # equation parameters
+mat = {'cx': 1450, 'cy': 700} # equation parameters
+
+def clean_data():
+	for j in range(0, ny):
+		for i in range(0, nx):
+			data_curr[j][i] = 0;
 
 def apply_initial_data():
-	for j in range(int(ny/4), int(ny/4 * 3)):
-		for i in range(int(nx/4), int(nx/4 * 3)):
+	for j in range(int(ny/3), int(ny/3 * 2)):
+		for i in range(int(nx/3), int(nx/3 * 2)):
 			data_curr[j][i] = u0;
 
 def save_data():
@@ -53,8 +58,13 @@ def save_data():
 	# Z = np.exp(-X**2 / 100 - Y**2 / 100) # for test function
 	X, Y = np.meshgrid(x, y)
 	Z = np.array(data_curr)
-	_, ax = plt.subplots()
-	ax.imshow(Z)
+	_, ax = plt.subplots(2, 1)
+	ax[1].imshow(Z)
+	# FIXME remove it, break calculations
+	clean_data()
+	apply_initial_data()
+	Z = np.array(data_curr)
+	ax[0].imshow(Z)
 	plt.show()
 
 if __name__ == '__main__':
@@ -69,6 +79,10 @@ if __name__ == '__main__':
 		for j in range(1, ny - 1): # Don't think much about boundaries
 			for i in range(1, nx - 1):
 				data_next[j][i] = data_curr[j][i] - mat['cx'] * (data_curr[j][i] - data_curr[j][i-1]) / dx * dt
+		# update time layer
+		for j in range(1, ny - 1): # Don't think much about boundaries
+			for i in range(1, nx - 1):
+				data_curr[j][i] = data_next[j][i]
 		# stepY
 		for i in range(1, nx - 1): # Don't think much about boundaries
 			for j in range(1, ny - 1):
